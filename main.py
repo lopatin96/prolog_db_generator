c4b9505ca4fdd8da4dd9d957093f115c91f08514
lopatin96 <ilopatin96@mail.ru>
2019-11-02 15:23:33 +0100

    finished

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 import csv
 
-with open('people.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+with open('data.csv') as csv_file:
+    with open('db.pl', 'w') as file:
+        csv_reader = csv.reader(csv_file, delimiter=';')
+        line_count = 0
+        facts = []
+        names = []
+        for row in csv_reader:
+            if line_count == 0:
+                row.pop(0)
+                file.write('get_questions(Q) :-\n')
+                file.write('    ')
+                file.write('Q = [' + ', '.join('\'{0}\''.format(w) for w in row) + '].\n\n')
+                line_count += 1
+            elif line_count == 1:
+                row.pop(0)
+                file.write('get_facts(F) :-\n')
+                file.write('    ')
+                facts = row
+                file.write('F = [' + ', '.join(row) + '].\n\n')
+                line_count += 1
+            else:
+                names.append(row.pop(0))
+                for index, fact in enumerate(row):
+                    if fact == '1':
+                        file.write(facts[index] + '(' + names[-1] + ').\n')
+                line_count += 1
+        file.write('get_people(P) :-\n')
+        file.write('    ')
+        file.write('P = [' + ', '.join(names) + '].')
+        print(f'Processed {line_count} lines.')
